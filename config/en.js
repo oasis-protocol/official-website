754287112080f20cac6690991edb99b8ed71c8cf
yyh1102 <yyh951102@163.com>
2020-07-08 16:11:39 +0800

    add english version

---
+++ config/en.js
@@ -0,0 +1,72 @@
+export default {
+  特性: "Features",
+  社区: "Community",
+  合作: "Contact",
+  接入文档: "Docs",
+  低门槛接入虚拟资产互通的应用生态:
+    "Low threshold access to the application ecosystem of virtual asset interoperability",
+  NFT资产跨应用互通: "NFT assets inter-application interoperability",
+  快速入门: "Quick Guide",
+  "THE OASIS协议是一套基于区块链的协议与工具套件。它实现了基于NFT（非同质化通证）的虚拟资产在不同合约间的转移与复用，并提供了封装优良的资产复用、解析工具包":
+    "THE OASIS protocol is a set of blockchain-based protocols and tool suites. It realizes the transfer and reuse of virtual assets based on NFT (Non-Fungible Token) among different contracts, and provides a well-packaged asset reuse and analysis toolkit",
+  现已支持: "Support",
+  standardAsset:
+    "THE OASIS has developed a <strong>FT/NFT</strong> with good scalability Standard interface, and provide a complete contract template",
+  通用资产标准: "Universal Asset Standard",
+  跨合约NFT转移: "Cross-contract NFT Transfer",
+  "THE OASIS实现了安全、原子的跨合约NFT转移，帮助您的资产穿梭在各种应用的合约层":
+    "THE OASIS implements secure and atomic cross-contract NFT transfers, helping your assets shuttle through the data layer of various applications",
+  定制化资产映射: "Customized asset mapping",
+  "您可以自由定制外来NFT在合约内的映射目标资产，以实现更灵活的资产复用":
+    "You can freely customize the mapping target assets of foreign NFT in the contract to achieve more flexible asset reuse",
+  密码学安全: "Cryptographic Security",
+  "数字签名保障资产完整性，防止篡改资产":
+    "Digital signatures guarantee asset integrity and prevent tampering",
+  "中转合约记录资产位置，防止伪造资产":
+    "Transfer contract records the location of assets to prevent counterfeit assets",
+  全生命周期追踪: "Full life cycle tracking",
+  "为什么要接入THE OASIS": "Why join in THE OASIS",
+  快速冷启动: "Quick Cold Start",
+  "复用已有应用的资产，快速吸引流量，加速应用冷启动":
+    "Reuse assets of existing applications, quickly attract traffic, and accelerate application cold start",
+  刺激消费: "Stimulate Consumption",
+  迫使玩家重复消费道具的时代即将过去:
+    "The era of forcing players to repeatedly consume props is about to pass",
+  "复用性强，使用场景更丰富的资产更容易受到玩家青睐":
+    "Strong reusability, assets with richer usage scenarios are more likely to be favored by players",
+  跨应用联合玩法: "Cross-application Gameplay",
+  带着CryptoKitties的宠物猫游览Decentraland的虚拟世界:
+    "Take a Crypto Kitty to visit Decentraland",
+  已接入的Dapp: "Connected Dapp",
+  "跨应用资产转移需要收费吗？":
+    "Is there a charge for cross-application asset transfer?",
+  "在合约执行层面只收取区块链交易的手续费，无其他任何费用。应用方也可以在上层构建灵活的付费方案，以拓展更多商业模式。":
+    "At the contract execution level, only the transaction fees are charged. The application can also build a flexible payment plan on the upper layer to explore more business models.",
+  "资产转移后会出现丢失情况吗？":
+    "Will there be a loss after the asset is transferred?",
+  "资产只在合约有安全漏洞或其他Bug的情况下可能会出现丢失。我们建议用户在审查目标应用的安全性并充分信任后，再进行资产转移的操作。":
+    "Assets may only be lost if there are security holes or other bugs in the contract. We recommend users review the security of the target application and fully trust it before proceeding with asset transfer operations.",
+  "合作方单方面退出生态，资产是否会受影响？":
+    "If the partner unilaterally withdraws from the ecosystem, will the assets be affected?",
+  "在合作方合约符合THE OASIS协议标准的情况下，任何资产在任何时候都可以转移回它最初的发行合约。":
+    "In the case where the partner contract complies with the THE OASIS protocol standard, any asset can be transferred back to its original issuance contract at any time.",
+  "接入协议后会对资产做什么限制？":
+    "What restrictions will be placed on assets after the access agreement?",
+  "协议仅对必要的数据字段做了标准化，其它任何操作均无限制，可由应用方自由扩展。":
+    "The protocol only standardizes the necessary data fields, and there are no restrictions on any other operations, which can be freely extended by the application side.",
+  "想与更多开发者、应用方交流？":
+    "WaWant to communicate with more developers and applications?",
+  好的灵感总是在交流的过程中产生:
+    "Good inspiration is always produced in the process of communication",
+  加入社区: "Join the community",
+  申请测试: "Apply for Early Access",
+  请通过邮件与我们取得联系: "Please contact us by email",
+  关于链坊: "About Byterum",
+  byterumInfo:
+    "Byterum is an innovative company focusing on the blockchain technology. We are committed to combining blockchain technology with products in the cultural and creative fields such as games, literature, and arts, promoting the token economy, reducing transaction friction costs, and protect users' assets.",
+  相关资源: "Resources",
+  竹录隐侠: "Panda Hero",
+  社区: "Community",
+  帮助: "Help",
+  报告Bug: "BUG Report",
+};
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 import Head from "next/head";
 import Particles from "react-particles-js";
+import i18n from "i18next";
+import { useTranslation, initReactI18next, Trans } from "react-i18next";
+import en from "../config/en";
+import { useState, useEffect } from "react";
+import { Form } from "react-bootstrap";
 
 const ParticlesParams = {
   particles: {
@@ -68,7 +73,43 @@ const ParticlesParams = {
   retina_detect: true,
 };
 
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: en,
+    },
+  },
+  lng: "en",
+  fallbackLng: "cn",
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
 export default function Home() {
+  const { t } = useTranslation();
+
+  const [lang, setLang] = useState("en");
+
+  useEffect(() => {
+    const cacheLang = localStorage.getItem("lang");
+    if (cacheLang && cacheLang !== lang) {
+      setLang(cacheLang);
+      i18n.changeLanguage(cacheLang);
+    }
+  });
+
+  function changeLang(e) {
+    const lang = e.target.value;
+    setLang(lang);
+    i18n.changeLanguage(lang);
+    localStorage.setItem("lang", lang);
+  }
+
+  let langText = "";
+  if (lang === "cn") langText = "中文";
+  else if (lang === "en") langText = "EN";
+
   return (
     <div className="container">
       <Head>
@@ -102,6 +143,10 @@ export default function Home() {
         {/* <!-- Bootstrap CSS / Color Scheme --> */}
         <link rel="stylesheet" href="css/default.css" id="theme-color" />
 
+        <script
+          src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
+          crossorigin
+        ></script>
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=UA-169399144-1"
@@ -139,7 +184,7 @@ export default function Home() {
               <ul className="navbar-nav mr-auto">
                 <li className="nav-item">
                   <a className="nav-link page-scroll" href="#features">
-                    特性
+                    {t("特性")}
                   </a>
                 </li>
                 <li className="nav-item">
@@ -154,16 +199,27 @@ export default function Home() {
                 </li>
                 <li className="nav-item">
                   <a className="nav-link page-scroll" href="#community">
-                    社区
+                    {t("社区")}
                   </a>
                 </li>
                 <li className="nav-item">
                   <a className="nav-link page-scroll" href="#contact">
-                    合作
+                    {t("合作")}
                   </a>
                 </li>
               </ul>
               <ul className="navbar-nav">
+                <li className="nav-item" style={{ marginTop: 3 }}>
+                  <Form.Control
+                    as="select"
+                    size="sm"
+                    value={lang}
+                    onChange={changeLang}
+                  >
+                    <option value="cn">中文</option>
+                    <option value="en">English</option>
+                  </Form.Control>
+                </li>
                 <li className="nav-item">
                   <a
                     className="nav-link"
@@ -180,7 +236,7 @@ export default function Home() {
                   target="_blank"
                   href="https://docs.theoasis.io"
                 >
-                  接入文档
+                  {t("接入文档")}
                 </a>
               </form>
             </div>
@@ -194,16 +250,16 @@ export default function Home() {
           <div className="row vh-md-100">
             <div className="col-md-5 ml-auto my-auto text-center text-md-left">
               <span className="text- text-muted">
-                低门槛接入虚拟资产互通的应用生态
+                {t("低门槛接入虚拟资产互通的应用生态")}
               </span>
               <h1 className="display-4 mt-2 mb-5">
-                THE OASIS - NFT资产跨应用互通
+                THE OASIS - {t("NFT资产跨应用互通")}
               </h1>
               <a
                 href="#about"
                 className="btn btn-secondary d-inline-flex flex-row align-items-center"
               >
-                快速入门
+                {t("快速入门")}
                 <em
                   data-feather="chevrons-right"
                   width="20"
@@ -225,15 +281,16 @@ export default function Home() {
           <div className="row">
             <div className="col-md-6 mr-auto text-center text-md-left lead">
               <p>
-                THE
-                OASIS协议是一套基于区块链的协议与工具套件。它实现了基于NFT（非同质化通证）的虚拟资产在不同合约间的转移与复用，并提供了封装优良的资产复用、解析工具包。
+                {t(
+                  "THE OASIS协议是一套基于区块链的协议与工具套件。它实现了基于NFT（非同质化通证）的虚拟资产在不同合约间的转移与复用，并提供了封装优良的资产复用、解析工具包"
+                )}
               </p>
             </div>
             <div className="col-md-5 ">
               <div className="container">
                 <div className="row">
                   <div className="col-md-6 mx-auto text-center">
-                    <h2>现已支持</h2>
+                    <h2>{t("现已支持")}</h2>
                   </div>
                 </div>
                 <div className="row mt-5 mb-7">
@@ -262,11 +319,13 @@ export default function Home() {
                       <i className="fa fa-file-text-o" />
                     </div>
                   </div>
-                  <h5>通用资产标准</h5>
-                  <p className="text-muted">
-                    THE OASIS制定了具备可扩展性良好的<strong>FT/NFT</strong>
-                    标准接口，并提供完善的合约模板
-                  </p>
+                  <h5>{t("通用资产标准")}</h5>
+                  <Trans i18nKey="standardAsset">
+                    <p className="text-muted">
+                      THE OASIS制定了具备可扩展性良好的<strong>FT/NFT</strong>
+                      标准接口，并提供完善的合约模板
+                    </p>
+                  </Trans>
                 </div>
                 <div className="col-md-6 box">
                   <div className="icon-box">
@@ -274,10 +333,11 @@ export default function Home() {
                       <i className="fa fa-exchange" />
                     </div>
                   </div>
-                  <h5>跨合约NFT转移</h5>
+                  <h5>{t("跨合约NFT转移")}</h5>
                   <p className="text-muted">
-                    THE
-                    OASIS实现了安全、原子的跨合约NFT转移，帮助您的资产穿梭在各种应用的合约层
+                    {t(
+                      "THE OASIS实现了安全、原子的跨合约NFT转移，帮助您的资产穿梭在各种应用的合约层"
+                    )}
                   </p>
                 </div>
                 <div className="col-md-6 box">
@@ -286,9 +346,11 @@ export default function Home() {
                       <i className="fa fa-edit" />
                     </div>
                   </div>
-                  <h5>定制化资产映射</h5>
+                  <h5>{t("定制化资产映射")}</h5>
                   <p className="text-muted">
-                    您可以自由定制外来NFT在合约内的映射目标资产，以实现更灵活的资产复用
+                    {t(
+                      "您可以自由定制外来NFT在合约内的映射目标资产，以实现更灵活的资产复用"
+                    )}
                   </p>
                 </div>
                 <div className="col-md-6 box">
@@ -297,11 +359,11 @@ export default function Home() {
                       <i className="fa fa-shield" />
                     </div>
                   </div>
-                  <h5>密码学安全</h5>
+                  <h5>{t("密码学安全")}</h5>
                   <ul className="text-muted list-unstyled">
-                    <li>数字签名保障资产完整性，防止篡改资产</li>
-                    <li>中转合约记录资产位置，防止伪造资产</li>
-                    <li>全生命周期追踪</li>
+                    <li>{t("数字签名保障资产完整性，防止篡改资产")}</li>
+                    <li>{t("中转合约记录资产位置，防止伪造资产")}</li>
+                    <li>{t("全生命周期追踪")}</li>
                   </ul>
                 </div>
               </div>
@@ -314,7 +376,7 @@ export default function Home() {
         <div className="container">
           <div className="row">
             <div className="col-md-6 mx-auto text-center">
-              <h2>为什么要接入THE OASIS？</h2>
+              <h2>{t("为什么要接入THE OASIS")}</h2>
             </div>
           </div>
           <div className="row mt-4">
@@ -324,9 +386,9 @@ export default function Home() {
                   <i className="fa fa-users" />
                 </div>
               </div>
-              <h5>快速冷启动</h5>
+              <h5>{t("快速冷启动")}</h5>
               <p className="text-muted">
-                复用已有应用的资产，快速吸引流量，加速应用冷启动
+                {t("复用已有应用的资产，快速吸引流量，加速应用冷启动")}
               </p>
             </div>
             <div className="col-md-4 mx-auto box text-center">
@@ -335,10 +397,10 @@ export default function Home() {
                   <i className="fa fa-shopping-bag" />
                 </div>
               </div>
-              <h5>刺激消费</h5>
+              <h5>{t("刺激消费")}</h5>
               <ul className="text-muted list-unstyled">
-                <li>迫使玩家重复消费道具的时代即将过去</li>
-                <li>复用性强，使用场景更丰富的资产更容易受到玩家青睐</li>
+                <li>{t("迫使玩家重复消费道具的时代即将过去")}</li>
+                <li>{t("复用性强，使用场景更丰富的资产更容易受到玩家青睐")}</li>
               </ul>
             </div>
             <div className="col-md-4 mx-auto box text-center">
@@ -347,9 +409,9 @@ export default function Home() {
                   <i className="fa fa-rocket" />
                 </div>
               </div>
-              <h5>跨应用联合玩法</h5>
+              <h5>{t("跨应用联合玩法")}</h5>
               <p className="text-muted">
-                带着CryptoKitties的宠物猫游览Decentraland的虚拟世界
+                {t("带着CryptoKitties的宠物猫游览Decentraland的虚拟世界")}
               </p>
             </div>
           </div>
@@ -361,12 +423,14 @@ export default function Home() {
         <div className="container">
           <div className="row">
             <div className="col-md-6 mx-auto text-center">
-              <h2>已接入的Dapp</h2>
+              <h2>{t("已接入的Dapp")}</h2>
             </div>
           </div>
           <div className="row mt-5 mb-7">
             <div className="col-md-5 mx-auto text-right">
-              <img src="img/pandahero.png" width="100" />
+              <a href="https://pandahero.io" target="_blank">
+                <img src="img/pandahero.png" width="100" />
+              </a>
             </div>
             <div className="col-md-5 mx-auto text-left">
               <img src="img/panda-logo.png" width="100" />
@@ -386,28 +450,35 @@ export default function Home() {
             <div className="col-md-10 mx-auto">
               <div className="row">
                 <div className="col-md-6 mb-5">
-                  <h6>跨应用资产转移需要收费吗？</h6>
+                  <h6>{t("跨应用资产转移需要收费吗？")}</h6>
                   <p className="text-muted">
-                    在合约执行层面只收取区块链交易的手续费，无其他任何费用。应用方也可以在上层构建灵活的付费方案，以拓展更多商业模式。
+                    {t(
+                      "在合约执行层面只收取区块链交易的手续费，无其他任何费用。应用方也可以在上层构建灵活的付费方案，以拓展更多商业模式。"
+                    )}
                   </p>
                 </div>
                 <div className="col-md-6 mb-5">
-                  <h6>资产转移后会出现丢失情况吗？</h6>
+                  <h6>{t("资产转移后会出现丢失情况吗？")}</h6>
                   <p className="text-muted">
-                    资产只在合约有安全漏洞或其他Bug的情况下可能会出现丢失。我们建议用户在审查目标应用的安全性并充分信任后，再进行资产转移的操作。
+                    {t(
+                      "资产只在合约有安全漏洞或其他Bug的情况下可能会出现丢失。我们建议用户在审查目标应用的安全性并充分信任后，再进行资产转移的操作。"
+                    )}
                   </p>
                 </div>
                 <div className="col-md-6 mb-5">
-                  <h6>合作方单方面退出生态，资产是否会受影响？</h6>
+                  <h6>{t("合作方单方面退出生态，资产是否会受影响？")}</h6>
                   <p className="text-muted">
-                    在合作方合约符合THE
-                    OASIS协议标准的情况下，任何资产在任何时候都可以转移回它最初的发行合约。
+                    {t(
+                      "在合作方合约符合THE OASIS协议标准的情况下，任何资产在任何时候都可以转移回它最初的发行合约。"
+                    )}
                   </p>
                 </div>
                 <div className="col-md-6 mb-5">
-                  <h6>接入协议后会对资产做什么限制？</h6>
+                  <h6>{t("接入协议后会对资产做什么限制？")}</h6>
                   <p className="text-muted">
-                    协议仅对必要的数据字段做了标准化，其它任何操作均无限制，可由应用方自由扩展。
+                    {t(
+                      "协议仅对必要的数据字段做了标准化，其它任何操作均无限制，可由应用方自由扩展。"
+                    )}
                   </p>
                 </div>
               </div>
@@ -424,13 +495,15 @@ export default function Home() {
               <div className="card p-4">
                 <div className="card-body d-md-flex flex-row align-items-center text-center text-md-left">
                   <div className="mb-4 mb-md-0">
-                    <h4>想与更多开发者、应用方交流？</h4>
+                    <h4>{t("想与更多开发者、应用方交流？")}</h4>
                     <span className="text-muted">
-                      好的灵感总是在交流的过程中产生
+                      {t("好的灵感总是在交流的过程中产生")}
                     </span>
                   </div>
                   <a className="ml-md-auto d-inline-flex flex-row align-items-center">
-                    <button className="btn btn-secondary">加入社区</button>
+                    <button className="btn btn-secondary">
+                      {t("加入社区")}
+                    </button>
                   </a>
                 </div>
               </div>
@@ -444,8 +517,8 @@ export default function Home() {
         <div className="container">
           <div className="row">
             <div className="col-md-12 my-md-auto text-center">
-              <h1 className="display text-white">申请测试</h1>
-              <p className="text-muted">请通过邮件与我们取得联系</p>
+              <h1 className="display text-white">{t("申请测试")}</h1>
+              <p className="text-muted">{t("请通过邮件与我们取得联系")}</p>
               <a
                 className="lead text-light"
                 href="mailto:the-oasis@byterum.com"
@@ -462,10 +535,10 @@ export default function Home() {
         <div className="container">
           <div className="row">
             <div className="col-sm-5 mr-auto">
-              <h6>关于 链坊科技</h6>
-              <p className="text-muted">
-                链坊科技是一家专注于区块链技术研发的创新科技公司。我们致力于将区块链技术与游戏、文学、艺术等文创领域产品结合，推广通证经济，降低交易摩擦成本，最大化保障用户的利益。
-              </p>
+              <h6>{t("关于链坊")}</h6>
+              <Trans i18nKey="byterumInfo" className="text-muted">
+                链坊是一家专注于区块链技术研发的创新科技公司。我们致力于将区块链技术与游戏、文学、艺术等文创领域产品结合，推广通证经济，降低交易摩擦成本，最大化保障用户的利益。
+              </Trans>
               <ul className="list-inline social social-rounded social-sm">
                 <li className="list-inline-item">
                   <a href="">
@@ -493,10 +566,10 @@ export default function Home() {
               </ul>
             </div>
             <div className="col-sm-2">
-              <h6>相关资源</h6>
+              <h6>{t("相关资源")}</h6>
               <ul className="list-unstyled">
                 <li>
-                  <a href="https://pandahero.io">竹录隐侠</a>
+                  <a href="https://pandahero.io">{t("竹录隐侠")}</a>
                 </li>
                 <li>
                   <a href="https://eos.io">EOSIO</a>
@@ -507,7 +580,7 @@ export default function Home() {
               </ul>
             </div>
             <div className="col-sm-2">
-              <h6>社区</h6>
+              <h6>{t("社区")}</h6>
               <ul className="list-unstyled">
                 <li>
                   <a href="#">Refer a friend</a>
@@ -518,7 +591,7 @@ export default function Home() {
               </ul>
             </div>
             <div className="col-sm-2">
-              <h6>帮助</h6>
+              <h6>{t("帮助")}</h6>
               <ul className="list-unstyled">
                 <li>
                   <a
@@ -529,18 +602,15 @@ export default function Home() {
                   </a>
                 </li>
                 <li>
-                  <a href="#">更新日志</a>
-                </li>
-                <li>
-                  <a href="#">报告Bug</a>
+                  <a href="#">{t("报告Bug")}</a>
                 </li>
               </ul>
             </div>
           </div>
           <div className="row mt-5">
             <div className="col-12 text-muted text-center small-xl">
-              &copy; {new Date().getFullYear()} 链坊科技 Byterum - All Rights
-              Reserved
+              THE OASIS&copy;2019-{new Date().getFullYear()} 链坊科技 Byterum -
+              All Rights Reserved
             </div>
           </div>
         </div>
